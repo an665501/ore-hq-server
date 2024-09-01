82b793546315b98699749f04100291162cfd0e74
an665501 <166208286+an665501@users.noreply.github.com>
2024-09-01 19:19:09 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -556,8 +556,8 @@ async fn ws_handler(
 
     let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs();
 
-    // Signed authentication message is only valid for 5 seconds
-    if (now - query_params.timestamp) >= 5 {
+    // Signed authentication message is only valid for 30 seconds
+    if (now - query_params.timestamp) >= 30 {
         return Err((StatusCode::UNAUTHORIZED, "Timestamp too old."));
     }
 
